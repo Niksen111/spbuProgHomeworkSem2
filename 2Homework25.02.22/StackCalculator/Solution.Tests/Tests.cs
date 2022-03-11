a967424706bfe4d663794c2c107a73638f6e6bdf
Niksen111 <nikiksen2003@gmail.com>
2022-03-11 23:06:47 +0300

    Writed SetUp for unit tests

--- 2Homework25.02.22/StackCalculator/Solution.Tests/Tests.cs
+++ 2Homework25.02.22/StackCalculator/Solution.Tests/Tests.cs
@@ -5,5 +5,11 @@ namespace StackCalculator.Solution.Tests;
 [TestFixture]
 public class Tests
 {
-    
+    private StackCalculator _calculator = new StackCalculator();
+
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new StackCalculator();
+    }
 }
