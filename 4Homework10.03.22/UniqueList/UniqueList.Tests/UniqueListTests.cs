af4ecaa4743293edd3fc82c8b01e5918692489c3
Niksen111 <nikiksen2003@gmail.com>
2022-04-01 02:21:22 +0300

    Added SetUp to UniqueList tests

--- 4Homework10.03.22/UniqueList/UniqueList.Tests/UniqueListTests.cs
+++ 4Homework10.03.22/UniqueList/UniqueList.Tests/UniqueListTests.cs
@@ -1,6 +1,14 @@
+using NUnit.Framework;
+
 namespace UniqueList.Tests;
 
 public class UniqueListTests
 {
-    
+    private UniqueList _list = new UniqueList();
+
+    [SetUp]
+    public void SetUp()
+    {
+        _list = new UniqueList();
+    }
 }
