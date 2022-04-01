776b1588c7e9289c67c35a5fed7a1a6ac7450ade
Niksen111 <nikiksen2003@gmail.com>
2022-04-01 23:33:36 +0300

    Added comments

--- 2Homework25.02.22/StackCalculator/StackCalculator/IStack.cs
+++ 2Homework25.02.22/StackCalculator/StackCalculator/IStack.cs
@@ -2,12 +2,25 @@ namespace StackCalculator;
 
 public interface IStack
 {
+    /// <summary>
+    /// Pushes the number to the head of the stack
+    /// </summary>
+    /// <param name="number">the number</param>
     void Push(float number);
-
+    
+    /// <summary>
+    /// Pop element from the head of the stack
+    /// </summary>
+    /// <returns></returns>
     float? Pop();
     
+    /// <summary>
+    /// Returns the element from the head of the stack
+    /// </summary>
     float? Top { get; }
-    
+    /// <summary>
+    /// Returns true if the stack is empty
+    /// </summary>
     bool IsEmpty { get; }
 
 }
