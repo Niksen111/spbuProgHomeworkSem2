9e671497dc4f429ac65f117b8d03dee1d06b382d
Niksen111 <nikiksen2003@gmail.com>
2022-04-14 23:25:18 +0300

    Writed functions

--- 6Homework24.03.22/MapFilterFold/MapFilterFold/IMyClass.cs
+++
@@ -1,6 +0,0 @@
-namespace MapFilterFold;
-
-public class IMyClass
-{
-    
-}
--- 6Homework24.03.22/MapFilterFold/MapFilterFold/MyClass.cs
+++ 6Homework24.03.22/MapFilterFold/MapFilterFold/MyClass.cs
@@ -2,5 +2,19 @@ namespace MapFilterFold;
 
 public class MyClass
 {
-    
+    public List<T> Map<T>(List<T> list, Func<T, T> func) => (List<T>) list.Select(func);
+
+    public List<T> Filter<T>(List<T> list, Predicate<T> predicate) => list.FindAll(predicate);
+
+    public T Fold<T>(List<T> list, T lastAcc, Func<(T, T), T> func)
+    {
+        var currentElem = list.GetEnumerator();
+        T acc = func((lastAcc, currentElem.Current));
+        while (currentElem.MoveNext())
+        {
+            acc = func((acc, currentElem.Current));
+        }
+        currentElem.Dispose();
+        return acc;
+    }
 }
