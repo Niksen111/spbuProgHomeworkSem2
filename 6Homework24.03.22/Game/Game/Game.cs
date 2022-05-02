ccc7e3b0b8802a2716d596a3269bc1116ca921a1
Niksen111 <nikiksen2003@gmail.com>
2022-05-02 15:58:42 +0300

    Writed Game

--- 6Homework24.03.22/Game/Game/Game.cs
+++ 6Homework24.03.22/Game/Game/Game.cs
@@ -2,5 +2,52 @@ namespace Game;
 
 public class Game
 {
+    public Game()
+    {
+        _currentPosition = (5, 20);
+        _lastMove = (0, 0);
+        _fieldSize = (12, 42);
+    }
     
+    private (int, int) _currentPosition;
+
+    private (int, int) _lastMove;
+    private readonly (int, int) _fieldSize;
+
+    private void Move(int x, int y)
+    {
+        if (_currentPosition.Item2 + x <= 0 || _currentPosition.Item2 + x >= _fieldSize.Item2 - 1 
+                                            || _currentPosition.Item1 + y < 0 ||
+                                            _currentPosition.Item1 + y >= _fieldSize.Item1 - 1)
+        {
+            return;
+        }
+        Console.SetCursorPosition(_currentPosition.Item2 - _lastMove.Item1, _currentPosition.Item1 - _lastMove.Item2);
+        Console.Write(" ");
+        Console.SetCursorPosition(_currentPosition.Item2 + x, _currentPosition.Item1 + y);
+        _currentPosition.Item2 += x;
+        _currentPosition.Item1 += y;
+        _lastMove = (x, y);
+        Console.Write("@");
+    }
+
+    public void Up(object sender, EventArgs args)
+    {
+        Move(0, -1);
+    }
+
+    public void Down(object sender, EventArgs args)
+    {
+        Move(0, 1);
+    }
+    
+    public void Left(object sender, EventArgs args)
+    {
+        Move(-1, 0);
+    }
+
+    public void Right(object sender, EventArgs args)
+    {
+        Move(1, 0);
+    }
 }
--- 6Homework24.03.22/Game/Game/Program.cs
+++ 6Homework24.03.22/Game/Game/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Game;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var file = File.ReadAllLines("../../../Map.txt");
+        foreach (var line in file)
+        {
+            Console.WriteLine(line);
+        }
+        Console.SetCursorPosition(20, 5);
+
+        
+        var eventLoop = new EventLoop();
+        var game = new Game();
+        eventLoop.UpHandler += game.Up!;
+        eventLoop.DownHandler += game.Down!;
+        eventLoop.LeftHandler += game.Left!;
+        eventLoop.RightHandler += game.Right!;
+        
+        eventLoop.Run();
+    }
+}
