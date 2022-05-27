76979dd73664c393f78840a9a211506e246ba963
Niksen111 <nikiksen2003@gmail.com>
2022-05-27 18:20:27 +0300

    Added MyCalculator class, added project with tests

---
+++ 7Homework31.03.22/Calculator/Calculator/MyCalculator.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace Calculator;
+
+public class MyCalculator
+{
+    private int _state;
+    private string _firstNumber;
+    private string _secondNumber;
+    private char _operation;
+
+    public MyCalculator()
+    {
+        _state = 1;
+    }
+
+    private string Operate()
+    {
+        switch (_operation)
+        {
+            case '+':
+                return (float.Parse(_firstNumber) + float.Parse(_secondNumber)).ToString();
+            case '-':
+                return (float.Parse(_firstNumber) - float.Parse(_secondNumber)).ToString();
+            case '*':
+                return (float.Parse(_firstNumber) * float.Parse(_secondNumber)).ToString();
+            case '/':
+                if (float.Parse(_secondNumber) < 0.000000001 && _secondNumber[0] == '+')
+                {
+                    throw new DivideByZeroException();
+                }
+                return (float.Parse(_firstNumber) / float.Parse(_secondNumber)).ToString();
+            default:
+                throw new DivideByZeroException();
+        }
+
+    }
+}
+
