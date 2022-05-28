6ca045515566cf29bdb1510bcb60fef5493bee36
Niksen111 <nikiksen2003@gmail.com>
2022-05-28 18:43:10 +0300

    Calculator works

--- 7Homework31.03.22/Calculator/Calculator/Form1.Designer.cs
+++ 7Homework31.03.22/Calculator/Calculator/Form1.Designer.cs
@@ -44,8 +44,6 @@
             this.button14 = new System.Windows.Forms.Button();
             this.button15 = new System.Windows.Forms.Button();
             this.button16 = new System.Windows.Forms.Button();
-            this.button17 = new System.Windows.Forms.Button();
-            this.button18 = new System.Windows.Forms.Button();
             this.button19 = new System.Windows.Forms.Button();
             this.button20 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -86,6 +84,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = ",";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -97,6 +96,7 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "=";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -108,6 +108,7 @@
             this.button5.TabIndex = 8;
             this.button5.Text = "1";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -119,6 +120,7 @@
             this.button6.TabIndex = 7;
             this.button6.Text = "2";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -130,6 +132,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "3";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -141,6 +144,7 @@
             this.button8.TabIndex = 5;
             this.button8.Text = "+";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -152,6 +156,7 @@
             this.button9.TabIndex = 12;
             this.button9.Text = "4";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -163,6 +168,7 @@
             this.button10.TabIndex = 11;
             this.button10.Text = "5";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -174,6 +180,7 @@
             this.button11.TabIndex = 10;
             this.button11.Text = "6";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -185,6 +192,7 @@
             this.button12.TabIndex = 9;
             this.button12.Text = "-";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
@@ -196,6 +204,7 @@
             this.button13.TabIndex = 16;
             this.button13.Text = "7";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button14
             // 
@@ -207,6 +216,7 @@
             this.button14.TabIndex = 15;
             this.button14.Text = "8";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // button15
             // 
@@ -218,6 +228,7 @@
             this.button15.TabIndex = 14;
             this.button15.Text = "9";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
             // button16
             // 
@@ -229,39 +240,19 @@
             this.button16.TabIndex = 13;
             this.button16.Text = "*";
             this.button16.UseVisualStyleBackColor = true;
-            // 
-            // button17
-            // 
-            this.button17.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button17.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button17.Location = new System.Drawing.Point(6, 106);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(89, 73);
-            this.button17.TabIndex = 20;
-            this.button17.Text = "1/x";
-            this.button17.UseVisualStyleBackColor = true;
-            // 
-            // button18
-            // 
-            this.button18.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button18.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button18.Location = new System.Drawing.Point(101, 106);
-            this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(89, 73);
-            this.button18.TabIndex = 19;
-            this.button18.Text = "x^2";
-            this.button18.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
             // 
             // button19
             // 
             this.button19.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button19.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button19.Location = new System.Drawing.Point(196, 106);
+            this.button19.Location = new System.Drawing.Point(6, 106);
             this.button19.Name = "button19";
-            this.button19.Size = new System.Drawing.Size(88, 73);
+            this.button19.Size = new System.Drawing.Size(278, 73);
             this.button19.TabIndex = 18;
             this.button19.Text = "C";
             this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
             // 
             // button20
             // 
@@ -273,6 +264,7 @@
             this.button20.TabIndex = 17;
             this.button20.Text = "/";
             this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
             // 
             // textBox1
             // 
@@ -292,8 +284,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(382, 499);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button17);
-            this.Controls.Add(this.button18);
             this.Controls.Add(this.button19);
             this.Controls.Add(this.button20);
             this.Controls.Add(this.button13);
@@ -340,8 +330,6 @@
         private Button button14;
         private Button button15;
         private Button button16;
-        private Button button17;
-        private Button button18;
         private Button button19;
         private Button button20;
         private TextBox textBox1;
--- 7Homework31.03.22/Calculator/Calculator/MyCalculator.cs
+++ 7Homework31.03.22/Calculator/Calculator/MyCalculator.cs
@@ -12,6 +12,8 @@ public class MyCalculator
     public MyCalculator()
     {
         _state = 1;
+        _firstNumber = "+0";
+        _secondNumber = "+0";
     }
 
     private string Operate()
@@ -31,9 +33,245 @@ public class MyCalculator
                 }
                 return (float.Parse(_firstNumber) / float.Parse(_secondNumber)).ToString();
             default:
-                throw new DivideByZeroException();
+                throw new InvalidOperationException();
         }
 
+    }
+
+    public string MakeAction(char action)
+    {
+        if (action == 'C')
+        {
+            _firstNumber = "+0";
+            _secondNumber = "+0";
+            _operation = '\0';
+            _state = 1;
+            return "0";
+        }
+        switch (_state)
+        {
+            case 1:
+                if (new List<char> { '+', '-', '*', '/' }.Contains(action))
+                {
+                    _operation = action;
+                    _state = 3;
+                    return "0";
+                }
+                else if (action >= '0' && action <= '9')
+                {
+                    if (_firstNumber.Length > 18)
+                    {
+                        return _firstNumber;
+                    }
+                    if (!(_firstNumber.Length == 2 && action == '0'))
+                    {
+                        _firstNumber += action;
+                        return float.Parse(_firstNumber).ToString();
+                    }
+                }
+                else if (action == ',')
+                {
+                    _state = 2;
+                    _firstNumber += action;
+                }
+                else if (action == '!')
+                {
+                    // ---------------
+                    _firstNumber = _firstNumber.Replace(_firstNumber[0], _firstNumber[0] == '+' ? '-' : '+');
+                    return float.Parse(_firstNumber).ToString();
+                }
+                else if (action == '=')
+                {
+                    _state = 5;
+                }
+
+                return _firstNumber;
+
+            case 2:
+                if (new List<char> { '+', '-', '*', '/' }.Contains(action))
+                {
+                    _operation = action;
+                    _state = 3;
+                    return "0";
+                }
+                else if (action >= '0' && action <= '9')
+                {
+                    if (_firstNumber.Length > 18)
+                    {
+                        return _firstNumber;
+                    }
+                    if (!(_firstNumber.Length == 2 && action == '0'))
+                    {
+                        _firstNumber += action;
+                        return float.Parse(_firstNumber).ToString();
+                    }
+                }
+                else if (action == '!')
+                {
+                    // ---------------
+                    _firstNumber = _firstNumber.Replace(_firstNumber[0], _firstNumber[0] == '+' ? '-' : '+');
+                    return float.Parse(_firstNumber).ToString();
+                }
+                else if (action == '=')
+                {
+                    _state = 5;
+                }
+
+                return _firstNumber;
+
+            case 3:
+                if (new List<char> { '+', '-', '*', '/' }.Contains(action))
+                {
+                    try
+                    {
+                        _firstNumber = Operate();
+                    }
+                    catch(DivideByZeroException exeption)
+                    {
+                        _firstNumber = "+0";
+                        _state = 1;
+                        _secondNumber = "+0";
+                        _operation = '\0';
+                        return "Divided by zero :(";
+                    }
+                    _secondNumber = "+0";
+                    _operation = action;
+                    return float.Parse(_secondNumber).ToString();
+                }
+                else if (action >= '0' && action <= '9')
+                {
+                    if (_secondNumber.Length > 18)
+                    {
+                        return _secondNumber;
+                    }
+                    if (!(_secondNumber.Length == 2 && action == '0'))
+                    {
+                        _secondNumber += action;
+                        return float.Parse(_secondNumber).ToString();
+                    }
+                }
+                else if (action == ',')
+                {
+                    _state = 4;
+                    _secondNumber += action;
+                }
+                else if (action == '!')
+                {
+                    // ---------------
+                    _secondNumber = _secondNumber.Replace(_secondNumber[0], _secondNumber[0] == '+' ? '-' : '+');
+                    return float.Parse(_secondNumber).ToString();
+                }
+                else if (action == '=')
+                {
+                    _state = 5;
+                    try
+                    {
+                        _firstNumber = Operate();
+                    }
+                    catch (DivideByZeroException exeption)
+                    {
+                        _firstNumber = "+0";
+                        _state = 1;
+                        _secondNumber = "+0";
+                        _operation = '\0';
+                        return "Divided by zero :(";
+                    }
+                    _secondNumber = "+0";
+                    return _firstNumber;
+                }
+
+                return _secondNumber;
+
+            case 4:
+                if (new List<char> { '+', '-', '*', '/' }.Contains(action))
+                {
+                    try
+                    {
+                        _firstNumber = Operate();
+                    }
+                    catch (DivideByZeroException exeption)
+                    {
+                        _firstNumber = "+0";
+                        _state = 1;
+                        _secondNumber = "+0";
+                        _operation = '\0';
+                        return "Divided by zero :(";
+                    }
+                    _secondNumber = "+0";
+                    _operation = action;
+                    _state = 3;
+                    return float.Parse(_secondNumber).ToString();
+                }
+                else if (action >= '0' && action <= '9')
+                {
+                    if (_secondNumber.Length > 18)
+                    {
+                        return _secondNumber;
+                    }
+                    if (!(_secondNumber.Length == 2 && action == '0'))
+                    {
+                        _secondNumber += action;
+                        return float.Parse(_secondNumber).ToString();
+                    }
+                }
+                else if (action == '!')
+                {
+                    // ---------------
+                    _secondNumber = _secondNumber.Replace(_secondNumber[0], _secondNumber[0] == '+' ? '-' : '+');
+                    return float.Parse(_secondNumber).ToString();
+                }
+                else if (action == '=')
+                {
+                    _state = 5;
+                    try
+                    {
+                        _firstNumber = Operate();
+                    }
+                    catch (DivideByZeroException exeption)
+                    {
+                        _firstNumber = "+0";
+                        _state = 1;
+                        _secondNumber = "+0";
+                        _operation = '\0';
+                        return "Divided by zero :(";
+                    }
+                    _secondNumber = "+0";
+                    return _firstNumber;
+                }
+
+                return _secondNumber;
+
+            case 5:
+                if (new List<char> { '+', '-', '*', '/' }.Contains(action))
+                {
+                    _operation = action;
+                    _state = 3;
+                    return "0";
+                }
+                else if (action == '!')
+                {
+                    // ---------------
+                    _firstNumber = _firstNumber.Replace(_firstNumber[0], _firstNumber[0] == '+' ? '-' : '+');
+                    return _firstNumber;
+                }
+                else if (action >= '0' && action <= '9')
+                {
+                    _firstNumber = "+" + action;
+                    _state = 1;
+                    return float.Parse(_firstNumber).ToString();
+                }
+                else if (action == ',')
+                {
+                    _firstNumber = "+0,";
+                    _state = 2;
+                    return "0,";
+                }
+
+                return _firstNumber;
+
+            default:
+                return _firstNumber;
+        }
     }
 }
 
