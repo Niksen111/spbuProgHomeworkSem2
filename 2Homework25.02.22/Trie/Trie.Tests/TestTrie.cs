60e879eedcb3c4328c7ec6646d62826e01573497
Niksen111 <nikiksen2003@gmail.com>
2022-05-31 14:38:00 +0300

    Fixed removing of words

--- 2Homework25.02.22/Trie/Trie.Tests/TestTrie.cs
+++ 2Homework25.02.22/Trie/Trie.Tests/TestTrie.cs
@@ -8,68 +8,83 @@ public class Tests
     [Test]
     public void TestTrieAddAndSize()
     {
-        Trie trie = new Trie();
-        bool result = trie.Size == 0;
-        trie.Add(new string("cat"));
-        result = result && trie.Size == 1;
-        trie.Add(new string("catland"));
-        result = result && trie.Size == 2;
-        trie.Add(new string("dog"));
-        result = result && trie.Size == 3;
-        trie.Add(new string(""));
-        result = result && trie.Size == 4;
-        
-        Assert.IsTrue(result);
+        var trie = new Trie();
+        Assert.AreEqual(trie.Size, 0);
+        trie.Add("cat");
+        Assert.AreEqual(trie.Size, 1);
+        trie.Add("catland");
+        Assert.AreEqual(trie.Size, 2);
+        trie.Add("dog");
+        Assert.AreEqual(trie.Size, 3);
+        trie.Add("");
+        Assert.AreEqual(trie.Size, 4);
     }
 
     [Test]
     public void TestTrieContains()
     {
-        Trie trie = new Trie();
-        trie.Add(new string("cat"));
-        trie.Add(new string("catland"));
-        trie.Add(new string("dog"));
-        trie.Add(new string(""));
-        Assert.IsTrue(trie.Contains("cat") 
-                      && trie.Contains("catland") 
-                      && trie.Contains("dog") 
-                      && trie.Contains("") 
-                      && !trie.Contains("mouse") 
-                      && !trie.Contains("catla"));
+        var trie = new Trie();
+        trie.Add("cat");
+        trie.Add("catland");
+        trie.Add("dog");
+        trie.Add("");
+        Assert.IsTrue(trie.Contains("cat"));
+        Assert.IsTrue(trie.Contains("catland"));
+        Assert.IsTrue(trie.Contains("dog"));
+        Assert.IsTrue(trie.Contains(""));
+        Assert.IsTrue(!trie.Contains("mouse"));
+        Assert.IsTrue(!trie.Contains("catla"));
     }
     
     [Test]
     public void TestTrieRemove()
     {
-        Trie trie = new Trie();
-        bool result = !trie.Remove("") && trie.Size == 0;
-        trie.Add(new string("cat"));
-        trie.Add(new string("catland"));
-        trie.Add(new string("dog"));
-        trie.Add(new string(""));
-        result = result && trie.Remove("") && trie.Size == 3
-                 && !trie.Contains("");
-        Assert.IsTrue(result && trie.Remove("cat")
-                             && trie.Remove("catland") && trie.Remove("dog")
-                             && !trie.Remove("mouse") && trie.Size == 0);
+        var trie = new Trie();
+        Assert.IsTrue(!trie.Remove(""));
+        Assert.AreEqual(trie.Size, 0);
+        trie.Add("cat");
+        trie.Add("catland");
+        trie.Add("dog");
+        trie.Add("");
+        Assert.IsTrue(trie.Remove(""));
+        Assert.AreEqual(trie.Size, 3);
+        Assert.IsTrue(!trie.Contains(""));
+        
+        Assert.IsTrue(trie.Remove("cat"));
+        Assert.IsTrue(trie.Remove("catland"));
+        Assert.IsTrue(trie.Remove("dog"));
+        Assert.IsFalse(trie.Remove("mouse"));
+        Assert.AreEqual(trie.Size, 0);
     }
     
     [Test]
     public void TestTrieHowManyStartsWithPrefix()
     {
-        Trie trie = new Trie();
-        trie.Add(new string("cat"));
-        trie.Add(new string("catland"));
-        trie.Add(new string("dog"));
-        trie.Add(new string(""));
-        trie.Add(new string("catland1"));
-        Assert.IsTrue(trie.HowManyStartsWithPrefix("") == 5
-                      && trie.HowManyStartsWithPrefix("d") == 1
-                      && trie.HowManyStartsWithPrefix("dog") == 1
-                      && trie.HowManyStartsWithPrefix("c") == 3
-                      && trie.HowManyStartsWithPrefix("catl") == 2
-                      && trie.HowManyStartsWithPrefix("catland") == 2
-                      && trie.HowManyStartsWithPrefix("catland1") == 1
-                      && trie.HowManyStartsWithPrefix("catland111") == 0);
+        var trie = new Trie();
+        trie.Add("cat");
+        trie.Add("catland");
+        trie.Add("dog");
+        trie.Add("");
+        trie.Add("catland1");
+        Assert.AreEqual(trie.HowManyStartsWithPrefix(""), 5);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("d"), 1);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("dog"), 1);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("c"), 3);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("catl"), 2);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("catland"), 2);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("catland1"), 1);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("catland111"), 0);
+    }
+
+    [Test]
+    public void Kek()
+    {
+        var trie = new Trie();
+        trie.Add("a");
+        trie.Add("aaa");
+        trie.Add("aab");
+        trie.Remove("aab");
+        
+        Assert.Pass();
     }
 }
--- 2Homework25.02.22/Trie/Trie/Trie.cs
+++ 2Homework25.02.22/Trie/Trie/Trie.cs
@@ -4,145 +4,148 @@ namespace Trie;
 /// data structure for storing a set of strings
 /// </summary>
 public class Trie
+{
+    private class Node
     {
-        private class Node
+        public Node()
         {
-            public Node()
-            {
-                this.Next = new Dictionary<char, Node>();
-            }
-            public short NumberOfWords;
-            public bool IsTerminal;
-            public Dictionary<char, Node> Next;
+            Next = new Dictionary<char, Node>();
         }
-        public Trie()
-        {
-            Head = new Node();
-        }
-
-        private Node Head;
-
-        /// <summary>
-        /// Number of words in the trie
-        /// </summary>
-        public int Size => Head.NumberOfWords;
         
-        /// <summary>
-        /// Added a string to the trie
-        /// </summary>
-        /// <param name="element">addable element</param>
-        /// <returns>true if didn't contain the string before</returns>
-        public bool Add(string element)
+        public short NumberOfWords;
+        public bool IsTerminal;
+        public Dictionary<char, Node> Next;
+    }
+    
+    public Trie()
+    {
+        _head = new Node();
+    }
+
+    private readonly Node _head;
+
+    /// <summary>
+    /// Number of words in the trie
+    /// </summary>
+    public int Size => _head.NumberOfWords;
+    
+    /// <summary>
+    /// Added a string to the trie
+    /// </summary>
+    /// <param name="element">addable element</param>
+    /// <returns>true if didn't contain the string before</returns>
+    public bool Add(string element)
+    {
+        var position = _head;
+        foreach (char c in element)
         {
-            Node position = Head;
+            if (!position.Next.ContainsKey(c))
+            {
+                var newNode = new Node();
+                position.Next.Add(c, newNode);
+            }
+
+            position = position.Next[c];
+        }
+
+        var result = !position.IsTerminal;
+        if (result)
+        {
+            position = _head;
             foreach (char c in element)
             {
-                if (!position.Next.ContainsKey(c))
-                {
-                    Node newNode = new Node();
-                    position.Next.Add(c, newNode);
-                }
-
-                position = position.Next[c];
-            }
-
-            bool result = !position.IsTerminal;
-            if (result)
-            {
-                position = Head;
-                foreach (char c in element)
-                {
-                    ++position.NumberOfWords;
-                    position = position.Next[c];
-                }
                 ++position.NumberOfWords;
-            }
-            position.IsTerminal = true;
-            return result;
-        }
-        
-        /// <summary>
-        /// Checks if the trie contains the line
-        /// </summary>
-        /// <param name="element">element being checked</param>
-        /// <returns>true if contained the line before</returns>
-        public bool Contains(string element)
-        {
-            Node position = Head;
-            foreach (char c in element)
-            {
-                if (!position.Next.ContainsKey(c))
-                {
-                    return false;
-                }
-
                 position = position.Next[c];
             }
-            return position.IsTerminal;
+            ++position.NumberOfWords;
         }
-        
-        /// <summary>
-        /// Removes the element
-        /// </summary>
-        /// <param name="element">removable element</param>
-        /// <returns>true if the element did contained</returns>
-        public bool Remove(string element)
+        position.IsTerminal = true;
+        return result;
+    }
+    
+    /// <summary>
+    /// Checks if the trie contains the line
+    /// </summary>
+    /// <param name="element">element being checked</param>
+    /// <returns>true if contained the line before</returns>
+    public bool Contains(string element)
+    {
+        var position = _head;
+        foreach (char c in element)
         {
-            Node position = Head;
-            Node lastWord = Head;
-            char nextSymbol = '\0';
-            foreach (char c in element)
-            {
-                if (!position.Next.ContainsKey(c))
-                {
-                    return false;
-                }
-
-                if (position.IsTerminal)
-                {
-                    lastWord = position;
-                    nextSymbol = c;
-                }
-                position = position.Next[c];
-            }
-
-            if (!position.IsTerminal)
+            if (!position.Next.ContainsKey(c))
             {
                 return false;
             }
-            
-            position = Head;
-            foreach (char c in element)
+
+            position = position.Next[c];
+        }
+        return position.IsTerminal;
+    }
+    
+    /// <summary>
+    /// Removes the element
+    /// </summary>
+    /// <param name="element">removable element</param>
+    /// <returns>true if the element did contained</returns>
+    public bool Remove(string element)
+    {
+        var position = _head;
+        var lastFork = _head;
+        char nextSymbol = '\0';
+        foreach (char c in element)
+        {
+            if (!position.Next.ContainsKey(c))
             {
-                --position.NumberOfWords;
-                position = position.Next[c];
+                return false;
             }
 
+            if (position.Next.Count > 1)
+            {
+                lastFork = position;
+                nextSymbol = c;
+            }
+            position = position.Next[c];
+        }
+
+        if (!position.IsTerminal)
+        {
+            return false;
+        }
+        
+        position = _head;
+        foreach (char c in element)
+        {
             --position.NumberOfWords;
-            position.IsTerminal = false;
-            if (position.Next.Count != 0)
-            {
-                return true;
-            }
+            position = position.Next[c];
+        }
 
-            lastWord.Next.Remove(nextSymbol);
+        --position.NumberOfWords;
+        position.IsTerminal = false;
+        if (position.Next.Count != 0)
+        {
             return true;
         }
-        /// <summary>number of the elements</summary>
-        /// <param name="prefix">the prefix</param>
-        /// <returns>number of words starts with the prefix</returns>
-        public int HowManyStartsWithPrefix(String prefix)
-        {
-            Node position = Head;
-            foreach (char c in prefix)
-            {
-                if (!position.Next.ContainsKey(c))
-                {
-                    return 0;
-                }
-
-                position = position.Next[c];
-            }
-            return position.NumberOfWords;
-        }
+        
+        lastFork.Next.Remove(nextSymbol);
+        return true;
     }
+    
+    /// <summary>number of the elements</summary>
+    /// <param name="prefix">the prefix</param>
+    /// <returns>number of words starts with the prefix</returns>
+    public int HowManyStartsWithPrefix(String prefix)
+    {
+        var position = _head;
+        foreach (char c in prefix)
+        {
+            if (!position.Next.ContainsKey(c))
+            {
+                return 0;
+            }
+
+            position = position.Next[c];
+        }
+        return position.NumberOfWords;
+    }
+}
