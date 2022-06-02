49072f6dabdb26c0b778d3cea5bcbe085069f8b4
Niksen111 <nikiksen2003@gmail.com>
2022-06-02 14:18:13 +0300

    Created project

---
+++ 8Homework07.04.22/SkipList/SkipList/SkipList.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+
+namespace SkipList;
+
+public class SkipList<T> : IList<T>
+{
+    public int Count { get; }
+
+    public bool IsReadOnly { get; }
+    
+    public void Add(T element)
+    {
+        
+    }
+
+    public void Clear()
+    {
+        
+    }
+
+    public bool Contains(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int IndexOf(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(int index, T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T this[int index]
+    {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
